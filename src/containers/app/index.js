4e3d58cbe378a8637a7eaaecd070907d6a1cce2f
Kyle Rothenbaum <krothenbaum@gmail.com>
2017-10-16 09:03:58 -0700

    Updated Readme

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -8,23 +8,14 @@ const App = () => (
 	<div>
 		<header>
 			<Link to="/map-game-client" className='header-link'>Restart Game</Link>
-			{/*<Link to="/home">Home</Link>
-			<Link to="/about-us">About</Link>
-			<Link to="/RandomCityMap">Random Cities</Link>*/}
 			<Link to="/map-game-client/scoreboard" className='header-link'>Score Board</Link>
 
 		</header>
 
 		<main>
 			<Route exact path="/map-game-client" component={Map} />
-			{/*<Route exact path="/home" component={Home} />
-			<Route exact path="/about-us" component={About} />
-			<Route exact path="/RandomCityMap" component={RandomCityMap} />*/}
 			<Route exact path="/map-game-client/scoreboard" component={ScoreBoard} />
-
 		</main>
 	</div>
 )
-
-
 export default App
